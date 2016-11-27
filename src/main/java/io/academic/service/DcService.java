434802c1edddafd1cc6016f06d43d7f65681ef15
Hüseyin Mert <huseyinin@gmail.com>
2016-11-27 18:29:30 +0300

    Main service Structure #3

---
+++ src/main/java/io/academic/service/DcService.java
@@ -0,0 +1,15 @@
+package io.academic.service;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+
+/**
+ * Converts record to DC and saves as Arcticle
+ */
+@Service
+public class DcService {
+}
